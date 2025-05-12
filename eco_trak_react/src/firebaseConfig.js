bdf147e3b6dd3aa40c140a8b2a0e2a4becced372
DevHades <zosobradogt@gmail.com>
2025-05-12 16:48:21 +0800

    sana gumana

---
+++ eco_trak_react/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3tLjWLusq-FJZKWeqpjC2-HvXwdvI_98",
+  authDomain: "ecotrak-fac7c.firebaseapp.com",
+  projectId: "ecotrak-fac7c",
+  storageBucket: "ecotrak-fac7c.firebasestorage.app",
+  messagingSenderId: "2977781169",
+  appId: "1:2977781169:web:9ae4ab4c7047eccb5dd990"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
